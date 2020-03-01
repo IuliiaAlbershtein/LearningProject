48526a8981106aab2fe0cc0ff466792dd1acfc2c
Iuliia Albershtein <iuliiaseredkina@gmail.com>
2020-03-01 15:49:37 +0100

    Task 1

--- LearningProject/Program.cs
+++ LearningProject/Program.cs
@@ -10,6 +10,44 @@ namespace LearningProject
     {
         static void Main(string[] args)
         {
+            Task1();
+        }
+        static void Task1()
+        {
+            // Max of 3 integers
+            int m = ReadInt();
+            int k = ReadInt();
+            int p = ReadInt();
+
+            int max = Max(m, k, p);
+            Console.WriteLine(max);
+
+
+            Console.ReadLine();
+        }
+        static int ReadInt()
+        {
+            string str = Console.ReadLine();
+            int integer = int.Parse(str);
+            return integer;
+        }
+        static int Max(int i1, int i2)
+        {
+            if (i1 > i2)
+            {
+                return i1;
+            }
+            else
+            {
+                return i2;
+            }
+        }
+
+        static int Max(int i1, int i2, int i3)
+        {
+            int max1 = Max(i1, i2);
+            int max2 = Max(max1, i3);
+            return max2;
         }
     }
 }
