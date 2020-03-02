3e9485cce8c70c065cc714622c20f5089b7570b4
Iuliia Albershtein <iuliiaseredkina@gmail.com>
2020-03-02 20:44:12 +0100

    Task3

--- LearningProject/Program.cs
+++ LearningProject/Program.cs
@@ -11,7 +11,8 @@ namespace LearningProject
         static void Main(string[] args)
         {
             // Task1();
-            Task2();
+            // Task2();
+            Task3();
         }
 
         static void Task2()
@@ -74,6 +75,74 @@ namespace LearningProject
             int max1 = Max(i1, i2);
             int max2 = Max(max1, i3);
             return max2;
+        }
+        static void Task3()
+        {
+            Console.WriteLine("What is the month now?");
+            int month = ReadInt();
+            Console.Write("It is ");
+
+            if (month == 1)
+            {
+                Console.WriteLine("January");
+            }
+            else
+            if (month == 2)
+            {
+                Console.WriteLine("February");
+            }
+            else
+            if (month == 3)
+            {
+                Console.WriteLine("March");
+            }
+            else
+            if (month == 4)
+            {
+                Console.WriteLine("April");
+            }
+            else
+            if (month == 5)
+            {
+                Console.WriteLine("May");
+            }
+            else
+            if (month == 6)
+            {
+                Console.WriteLine("June");
+            }
+            else
+            if (month == 7)
+            {
+                Console.WriteLine("July");
+            }
+            else
+            if (month == 8)
+            {
+                Console.WriteLine("August");
+            }
+            else
+            if (month == 9)
+            {
+                Console.WriteLine("September");
+            }
+            else
+            if (month == 10)
+            {
+                Console.WriteLine("October");
+            }
+            else
+            if (month == 11)
+            {
+                Console.WriteLine("November");
+            }
+            else
+            if (month == 12)
+            {
+                Console.WriteLine("December");
+            }
+            Console.ReadLine();
+
         }
     }
 }
