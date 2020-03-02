1e3d726f9f9f763f19c3330152831b2fd2d7cbd5
Iuliia Albershtein <iuliiaseredkina@gmail.com>
2020-03-02 20:57:23 +0100

    Task 4

--- LearningProject/Program.cs
+++ LearningProject/Program.cs
@@ -12,69 +12,18 @@ namespace LearningProject
         {
             // Task1();
             // Task2();
-            Task3();
+            // Task3();
+            Task4();
         }
-
-        static void Task2()
+        static void Task4()
         {
-            int width = ReadInt();
-            int height = ReadInt();
-            int j = 0;
-
-            while(j < height)
-            {
-                j = j + 1;
-                int i = 0;
-                while (i < width)
-                {
-                    i = i + 1;
-                    Console.Write("*");
-                }
-                Console.Write("\n");
-            }
-
-           
-            
-            Console.ReadLine();
-
-        }
-
-        static void Task1()
-        {
-            // Max of 3 integers
             int m = ReadInt();
             int k = ReadInt();
             int p = ReadInt();
 
-            int max = Max(m, k, p);
-            Console.WriteLine(max);
-
-
+            int summax = SumMax(m, k, p);
+            Console.WriteLine(summax);
             Console.ReadLine();
-        }
-        static int ReadInt()
-        {
-            string str = Console.ReadLine();
-            int integer = int.Parse(str);
-            return integer;
-        }
-        static int Max(int i1, int i2)
-        {
-            if (i1 > i2)
-            {
-                return i1;
-            }
-            else
-            {
-                return i2;
-            }
-        }
-
-        static int Max(int i1, int i2, int i3)
-        {
-            int max1 = Max(i1, i2);
-            int max2 = Max(max1, i3);
-            return max2;
         }
         static void Task3()
         {
@@ -142,6 +91,97 @@ namespace LearningProject
                 Console.WriteLine("December");
             }
             Console.ReadLine();
+        }
+
+        static void Task2()
+        {
+            int width = ReadInt();
+            int height = ReadInt();
+            int j = 0;
+
+            while (j < height)
+            {
+                j = j + 1;
+                int i = 0;
+                while (i < width)
+                {
+                    i = i + 1;
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+
+        static void Task1()
+        {
+            // Max of 3 integers
+            int m = ReadInt();
+            int k = ReadInt();
+            int p = ReadInt();
+
+            int max = Max(m, k, p);
+            Console.WriteLine(max);
+
+
+            Console.ReadLine();
+        }
+
+        static int ReadInt()
+        {
+            string str = Console.ReadLine();
+            int integer = int.Parse(str);
+            return integer;
+        }
+        static int Max(int i1, int i2)
+        {
+            if (i1 > i2)
+            {
+                return i1;
+            }
+            else
+            {
+                return i2;
+            }
+        }
+
+        static int Max(int i1, int i2, int i3)
+        {
+            int max1 = Max(i1, i2);
+            int max2 = Max(max1, i3);
+            return max2;
+        }
+        static int Sum(int i1, int i2)
+        {
+            int sum = i1 + i2;
+            return sum;
+        }
+        static int SumMax(int i1, int i2, int i3)
+        {
+            int sum1 = Sum(i1, i2);
+            int sum2 = Sum(i1, i3);
+            int sum3 = Sum(i2, i3);
+            if (sum1 > sum2)
+            {
+                return sum1;
+            }
+            else
+            {
+                if (sum2 > sum3)
+                {
+                    return sum2;
+                }
+                else
+                {
+                    return sum3;
+                }
+            }
+
+
 
         }
     }
