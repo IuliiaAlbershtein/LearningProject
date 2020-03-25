0683c1946784c58bdfb3da4d7bad2f2f25b62d64
Iuliia Albershtein <iuliiaseredkina@gmail.com>
2020-03-25 20:40:59 +0100

    LinkedList

--- LearningProject/LinkedList.cs
+++ LearningProject/LinkedList.cs
@@ -6,79 +6,74 @@ using System.Threading.Tasks;
 
 namespace LearningProject
 {
-    // Array
-    // [xxxx]
-    // [xxxxxx          ]
-
-    // Node
-    //*[ ] -> [ ] -> [ ] -> null
-    //  5      3      7
-    //*[ ] -> [ ] -> [ ] -> [ ] -> null
-    //  9      5      3      7
-
-    class Node
+//создание нового класса Node
+class Node
     {
+        //в нем существуют значения
         private int value;
+        //конструкция getter/setter для возвращения/становления значения
         public int Value
         {
             get { return value; }
+            //при вызове сеттера можно расширить условия
             set { this.value = value; }
         }
-
+        //ссылка на следующее значение
         private Node next;
         public Node Next
         {
             get { return next; }
             set { next = value; }
         }
-
+        //расширение или упрощение доступной функционалиты под свои требования
         public override string ToString()
         {
+            //неуверена в понимании синтаксиа???????????
             return $"{value}";
         }
     }
-
     class LinkedList
     {
+        //значение стоящее в начале цепочки
         private Node head;
 
-        public LinkedList()
-        {
-        }
-
+        //добавление значения вначале
         public void AddFirst(int value)
         {
             var newNode = new Node();
+            //создаем указатель от вставленного звена на тоб
+            //которое было первым
             newNode.Next = head;
             newNode.Value = value;
-
+            //назначение нового члена цепочки первым
             head = newNode;
         }
-
-        //  ||
-        //  \/
-        //*[ ] -> [ ] -> [ ] -> [ ] -> null
-        //  9      5      3      7
-        //*[ ] -> [ ] -> [ ] -> null
-        //  5      3      7
-
+        //удаление первого значения
         public void RemoveFirst()
         {
             head = head.Next;
         }
-
+        //удаление последнего значения - проба
         public void RemoveLast()
         {
-
+            var current = head;
+            while (current != null)
+            {
+                current = current.Next;
+                if (current.Next == null)
+                {
+                    current = null;
+                }
+            }
         }
-
-        // todo: make variable
+        //подсчет узлов в цепочке
         public int Count
         {
             get
             {
                 var count = 0;
                 var node = head;
+                //двигаемся по цепочке
                 while (node != null)
                 {
                     count += 1;
@@ -87,7 +82,7 @@ namespace LearningProject
                 return count;
             }
         }
-
+        //получение значения под определенным индексом
         public int GetValue(int index)
         {
             var i = 0;
@@ -98,12 +93,13 @@ namespace LearningProject
                 i += 1;
                 if (i == index)
                 {
+                    //вернуть значение в данном узле
                     return current.Value;
                 }
             }
             return -1;
         }
-
+        //выполнить действие для каждого
         public void ForEach(Action<int> action)
         {
             var current = head;
@@ -113,17 +109,42 @@ namespace LearningProject
                 current = current.Next;
             }
         }
-
+        //вложить указанное значение в указанный индекс
         public void InsertAt(int index, int value)
         {
-
+            var newNode = new Node();
+            newNode.Value = value;
+            var current = head;
+            var i = 0;
+            while (current != null)
+            {
+                current = current.Next;
+                i += 1;
+                if (i + 1 == index)
+                {
+                    newNode.Next = current.Next;
+                    current.Next = newNode;
+                }
+            }
         }
-
+        //удалить значение в индексе
         public void RemoveAt(int index)
         {
+            var i = 0;
+            var current = head;
+            while (current != null)
+            {
+                current = current.Next;
+                i += 1;
+                if (i == index - 1)
+                {
+                    var temp = current.Next;
+                    current.Next = temp.Next;
+                }
+            }
 
         }
-
+        //добавить новое последнее значение
         public void AddLast()
         {
 
